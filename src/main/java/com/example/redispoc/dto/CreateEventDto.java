12ffab7604185c1345f5f168434a7fec49c101db
Travis Rogers <travis.rogers@wnins.com>
2020-10-06 17:26:57 -0500

    add event dtos

---
+++ src/main/java/com/example/redispoc/dto/CreateEventDto.java
@@ -0,0 +1,45 @@
+package com.example.redispoc.dto;
+
+public class CreateEventDto {
+
+	private String name;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CreateEventDto other = (CreateEventDto) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "CreateEventDto [name=" + name + "]";
+	}
+
+}
---
+++ src/main/java/com/example/redispoc/dto/EventDto.java
@@ -0,0 +1,64 @@
+package com.example.redispoc.dto;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class EventDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private UUID id;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EventDto other = (EventDto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "EventDto [name=" + name + ", id=" + id + "]";
+	}
+
+}
